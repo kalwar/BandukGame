e6fb72dff2ea274e35b2a8a1f11870efcd7074b4
Santosh Kalwar <skalwar@Santoshs-MacBook-Pro.local>
2018-07-09 23:26:21 +0300

    Initial commit

---
+++ src/com/appketo/banduk/Line.java
@@ -0,0 +1,9 @@
+package com.appketo.banduk;
+
+import android.graphics.Point;
+
+public class Line
+{
+   public Point start = new Point(); // start Point--(0,0) by default
+   public Point end = new Point(); // end Point--(0,0) by default
+} // end class Line
